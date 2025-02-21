db4536af8201d739dec238e7b43548da9ee8e0a0
coob <koosanghwi@gmail.com>
2025-02-21 20:22:41 +0900

    ver 1.0104

--- data_ymir/mymodule/stop_event18.py
+++ data_ymir/mymodule/stop_event18.py
@@ -30,7 +30,7 @@ def _stop_please(cla):
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
             imgs_ = imgs_set_(70, 600, 350, 770, cla, img, 0.7)
             if imgs_ is not None and imgs_ != False:
-                print("18_1")
+                print("18_2")
                 click_pos_reg(imgs_.x, imgs_.y, cla)
                 time.sleep(1)
         # full_path = "c:\\my_games\\" + str(v_.game_folder) + "\\" + str(v_.data_folder) + "\\imgs\\18\\18_2.PNG"
--- data_ymir/mymodule/test_.py
+++ data_ymir/mymodule/test_.py
@@ -37,19 +37,12 @@ def go_test():
     elif cla == "six":
         plus = 960 * 5
     try:
-        full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\action\\out_check\\out_check.PNG"
+        full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\18\\18_2.PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(0, 800, 60, 900, cla, img, 0.7)
+        imgs_ = imgs_set_(70, 600, 350, 770, cla, img, 0.7)
         if imgs_ is not None and imgs_ != False:
-            print("out_check")
-
-        full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\action\\confirm_all\\confirm_1.PNG"
-        img_array = np.fromfile(full_path, np.uint8)
-        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(400, 540, 600, 660, cla, img, 0.7)
-        if imgs_ is not None and imgs_ != False:
-            print("confirm_1")
+            print("18_2")
 
 
     except Exception as e:
