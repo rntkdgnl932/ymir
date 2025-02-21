5ace1c0486acdb664672876fc458d64b562e30c4
coob <koosanghwi@gmail.com>
2025-02-21 20:29:10 +0900

    ver 1.0104

--- data_ymir/mymodule/character_select_and_game_start.py
+++ data_ymir/mymodule/character_select_and_game_start.py
@@ -57,6 +57,7 @@ def game_start_screen(cla, character_id):
                         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
                         imgs_ = imgs_set_(800, 980, 960, 1040, cla, img, 0.8)
                         if imgs_ is not None and imgs_ != False:
+                            _stop_please(cla)
                             ready = True
                         else:
                             full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\character_start\\ready_cancle_btn2.PNG"
@@ -414,6 +415,7 @@ def game_ready(cla):
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
                     imgs_ = imgs_set_(800, 980, 960, 1040, cla, img, 0.8)
                     if imgs_ is not None and imgs_ != False:
+                        _stop_please(cla)
                         game_ready = True
                 if game_ready == True:
                     wait_game(cla)
@@ -471,6 +473,7 @@ def wait_game(cla):
                         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
                         imgs_ = imgs_set_(800, 980, 960, 1040, cla, img, 0.8)
                         if imgs_ is not None and imgs_ != False:
+                            _stop_please(cla)
                             click_pos_2(500, 400, cla)
 
                         else:
@@ -526,6 +529,7 @@ def download_game(cla):
             if imgs_ is not None and imgs_ != False:
                 game_ready == False
                 print("downloading 2", game_ready_count, "초")
+                _stop_please(cla)
             else:
                 full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\character_start\\downloading.PNG"
                 img_array = np.fromfile(full_path, np.uint8)
