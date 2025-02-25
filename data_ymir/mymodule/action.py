14e251fa685e294a8d76a4c8fbc219ec181e57a7
coob <koosanghwi@gmail.com>
2025-02-26 08:57:39 +0900

    ver 1.02705

--- data_ymir/mymodule/action.py
+++ data_ymir/mymodule/action.py
@@ -200,7 +200,7 @@ def confirm_all(cla):
                     full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\request\\request_confirm.PNG"
                     img_array = np.fromfile(full_path, np.uint8)
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(480, 655, 590, 710, cla, img, 0.85)
+                    imgs_ = imgs_set_(100, 655, 960, 710, cla, img, 0.85)
                     if imgs_ is not None and imgs_ != False:
                         print("request_confirm", imgs_)
                         click_pos_reg(imgs_.x, imgs_.y, cla)
