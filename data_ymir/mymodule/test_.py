e10f1082d709b4179c0073c90a1a95fc5c503bfd
coob <koosanghwi@gmail.com>
2025-02-27 04:12:18 +0900

    ver 1.02804

--- data_ymir/mymodule/test_.py
+++ data_ymir/mymodule/test_.py
@@ -50,7 +50,7 @@ def go_test():
         full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\game_check\\black_screen.PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(0, 0, 960, 1040, cla, img, 0.97)
+        imgs_ = imgs_set_(0, 0, 960, 1040, cla, img, 0.99)
         if imgs_ is not None and imgs_ != False:
             print("black_screen")
 
