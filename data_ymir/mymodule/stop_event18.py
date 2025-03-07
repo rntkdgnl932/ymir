2fee6961033cff8159d443164c3992f219bbf585
coob <koosanghwi@gmail.com>
2025-03-07 12:37:25 +0900

    ver 1.035

--- data_ymir/mymodule/stop_event18.py
+++ data_ymir/mymodule/stop_event18.py
@@ -18,6 +18,60 @@ def _stop_please(cla):
     try:
         print("_stop_please")
 
+        is_18 = False
+        while is_18 is False:
+            is_18 = True
+            full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\18\\18_1.PNG"
+            img_array = np.fromfile(full_path, np.uint8)
+            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+            imgs_ = imgs_set_(70, 600, 350, 770, cla, img, 0.7)
+            if imgs_ is not None and imgs_ != False:
+                print("18_1")
+                click_pos_reg(imgs_.x, imgs_.y, cla)
+                is_18 = False
+                time.sleep(1)
+            else:
+                full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\18\\18_2.PNG"
+                img_array = np.fromfile(full_path, np.uint8)
+                img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                imgs_ = imgs_set_(70, 600, 350, 770, cla, img, 0.7)
+                if imgs_ is not None and imgs_ != False:
+                    print("18_2")
+                    click_pos_reg(imgs_.x, imgs_.y, cla)
+                    is_18 = False
+                    time.sleep(1)
+                else:
+                    full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\18\\18_3.PNG"
+                    img_array = np.fromfile(full_path, np.uint8)
+                    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                    imgs_ = imgs_set_(790, 350, 860, 410, cla, img, 0.8)
+                    if imgs_ is not None and imgs_ != False:
+                        print("18_3")
+                        click_pos_reg(imgs_.x, imgs_.y, cla)
+                        is_18 = False
+                        time.sleep(1)
+                    else:
+                        full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\18\\event_1818.PNG"
+                        img_array = np.fromfile(full_path, np.uint8)
+                        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                        imgs_ = imgs_set_(790, 350, 860, 410, cla, img, 0.8)
+                        if imgs_ is not None and imgs_ != False:
+                            print("event_1818")
+                            click_pos_reg(imgs_.x, imgs_.y, cla)
+                            is_18 = False
+                            time.sleep(1)
+                        else:
+                            full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\18\\18_4.PNG"
+                            img_array = np.fromfile(full_path, np.uint8)
+                            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                            imgs_ = imgs_set_(790, 350, 860, 410, cla, img, 0.8)
+                            if imgs_ is not None and imgs_ != False:
+                                print("18_4")
+                                click_pos_reg(imgs_.x, imgs_.y, cla)
+                                is_18 = False
+                                time.sleep(1)
+            time.sleep(1)
+
         full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\character_start\\logout.PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
@@ -37,51 +91,6 @@ def _stop_please(cla):
                 print("clean sreenresult_schedule", result_schedule)
                 character_id = result_schedule[0][1]
                 game_start_screen(cla, character_id)
-
-        full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\18\\18_1.PNG"
-        img_array = np.fromfile(full_path, np.uint8)
-        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(70, 600, 350, 770, cla, img, 0.7)
-        if imgs_ is not None and imgs_ != False:
-            print("18_1")
-            click_pos_reg(imgs_.x, imgs_.y, cla)
-            time.sleep(1)
-        else:
-            full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\18\\18_2.PNG"
-            img_array = np.fromfile(full_path, np.uint8)
-            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-            imgs_ = imgs_set_(70, 600, 350, 770, cla, img, 0.7)
-            if imgs_ is not None and imgs_ != False:
-                print("18_2")
-                click_pos_reg(imgs_.x, imgs_.y, cla)
-                time.sleep(1)
-            else:
-                full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\18\\18_3.PNG"
-                img_array = np.fromfile(full_path, np.uint8)
-                img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(790, 350, 860, 410, cla, img, 0.8)
-                if imgs_ is not None and imgs_ != False:
-                    print("18_3")
-                    click_pos_reg(imgs_.x, imgs_.y, cla)
-                    time.sleep(1)
-                else:
-                    full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\18\\event_1818.PNG"
-                    img_array = np.fromfile(full_path, np.uint8)
-                    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(790, 350, 860, 410, cla, img, 0.8)
-                    if imgs_ is not None and imgs_ != False:
-                        print("event_1818")
-                        click_pos_reg(imgs_.x, imgs_.y, cla)
-                        time.sleep(1)
-                    else:
-                        full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\18\\18_4.PNG"
-                        img_array = np.fromfile(full_path, np.uint8)
-                        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                        imgs_ = imgs_set_(790, 350, 860, 410, cla, img, 0.8)
-                        if imgs_ is not None and imgs_ != False:
-                            print("18_4")
-                            click_pos_reg(imgs_.x, imgs_.y, cla)
-                            time.sleep(1)
         # full_path = "c:\\my_games\\" + str(v_.game_folder) + "\\" + str(v_.data_folder) + "\\imgs\\18\\18_2.PNG"
         # img_array = np.fromfile(full_path, np.uint8)
         # img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
