adfe1941157c1bb97a7f58f38eba3faec3ede44a
coob <koosanghwi@gmail.com>
2025-03-14 01:21:15 +0900

    ver 1.03801

--- data_ymir/mymodule/tuto.py
+++ data_ymir/mymodule/tuto.py
@@ -229,7 +229,7 @@ def tuto_click(cla):
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
             imgs_ = imgs_set_(830, 60, 960, 130, cla, img, 0.7)
             if imgs_ is not None and imgs_ != False:
-                print("ready_quest2")
+                print("ready_quest2", imgs_)
                 click_pos_reg(imgs_.x, imgs_.y, cla)
                 clicked = True
             else:
@@ -238,7 +238,7 @@ def tuto_click(cla):
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
                 imgs_ = imgs_set_(830, 60, 960, 130, cla, img, 0.7)
                 if imgs_ is not None and imgs_ != False:
-                    print("ready_quest")
+                    print("ready_quest", imgs_)
                     click_pos_reg(imgs_.x, imgs_.y, cla)
                     clicked = True
 
