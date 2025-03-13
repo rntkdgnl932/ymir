f161d650a31b360fa1e45a03a1bae9fbe1774240
coob <koosanghwi@gmail.com>
2025-03-13 13:02:04 +0900

    ver 1.03701

--- data_ymir/mymodule/action.py
+++ data_ymir/mymodule/action.py
@@ -274,7 +274,7 @@ def confirm_all(cla):
         full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\title\\pass.PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(420, 350, 540, 400, cla, img, 0.9)
+        imgs_ = imgs_set_(400, 350, 550, 400, cla, img, 0.8)
         if imgs_ is not None and imgs_ != False:
             print("confirm_all : pass")
             cancle_all(cla)
@@ -374,7 +374,7 @@ def cancle_all(cla):
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
         imgs_ = imgs_set_(340, 530, 480, 750, cla, img, 0.7)
         if imgs_ is not None and imgs_ != False:
-            print("cancle_btn")
+            print("cancle_btn", imgs_)
             click_pos_reg(imgs_.x, imgs_.y, cla)
             is_cancle = True
         else:
