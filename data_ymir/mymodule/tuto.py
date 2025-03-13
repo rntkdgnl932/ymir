d8ce954abbc00b5ce06b034c7642829ca015b9bf
coob <koosanghwi@gmail.com>
2025-03-14 02:02:36 +0900

    ver 1.03802

--- data_ymir/mymodule/tuto.py
+++ data_ymir/mymodule/tuto.py
@@ -271,7 +271,7 @@ def tuto_click(cla):
                             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
                             imgs_ = imgs_set_(300, 400, 700, 800, cla, img, 0.7)
                             if imgs_ is not None and imgs_ != False:
-                                print("im_not_move_notice_2")
+                                print("im_not_move_notice_2 ")
                                 im_move_not = True
                             else:
                                 full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\tuto\\grow\\not_ing_notice.PNG"
