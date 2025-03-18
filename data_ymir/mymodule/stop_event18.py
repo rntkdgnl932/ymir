3c5d961fe49e010994dc2901a74d1e11ec64dcb0
coob <koosanghwi@gmail.com>
2025-03-18 11:16:46 +0900

    ver 1.04

--- data_ymir/mymodule/stop_event18.py
+++ data_ymir/mymodule/stop_event18.py
@@ -12,9 +12,8 @@ sys.path.append('C:/my_games/' + str(v_.game_folder) + '/' + str(v_.data_folder)
 def _stop_please(cla):
     import numpy as np
     import cv2
-    from function_game import imgs_set_, click_pos_reg, click_pos_2
-    from schedule import myQuest_play_check
-    from character_select_and_game_start import game_start_screen
+    from function_game import imgs_set_, click_pos_reg
+    from game_check import check_start
     try:
         print("_stop_please")
 
@@ -72,34 +71,8 @@ def _stop_please(cla):
                                 time.sleep(1)
             time.sleep(1)
 
-        # full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\character_start\\logout.PNG"
-        # img_array = np.fromfile(full_path, np.uint8)
-        # img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        # imgs_ = imgs_set_(800, 970, 960, 1040, cla, img, 0.8)
-        # if imgs_ is not None and imgs_ != False:
-        #     click_pos_2(500, 400, cla)
-        #     result_schedule = myQuest_play_check(cla, "check")
-        #     print("clean sreenresult_schedule", result_schedule)
-        #     character_id = result_schedule[0][1]
-        #     game_start_screen(cla, character_id)
-        # else:
-        #     full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\character_start\\logout2.PNG"
-        #     img_array = np.fromfile(full_path, np.uint8)
-        #     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        #     imgs_ = imgs_set_(800, 970, 960, 1040, cla, img, 0.8)
-        #     if imgs_ is not None and imgs_ != False:
-        #         click_pos_2(500, 400, cla)
-        #         result_schedule = myQuest_play_check(cla, "check")
-        #         print("clean sreenresult_schedule", result_schedule)
-        #         character_id = result_schedule[0][1]
-        #         game_start_screen(cla, character_id)
-        # full_path = "c:\\my_games\\" + str(v_.game_folder) + "\\" + str(v_.data_folder) + "\\imgs\\18\\18_2.PNG"
-        # img_array = np.fromfile(full_path, np.uint8)
-        # img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        # imgs_ = imgs_set_(70, 670, 300, 770, cla, img, 0.7)
-        # if imgs_ is not None and imgs_ != False:
-        #     print("18_1")
-        #     click_pos_reg(imgs_.x, imgs_.y, cla)
+        print("stop plz, check_start")
+        check_start(cla)
 
 
 
