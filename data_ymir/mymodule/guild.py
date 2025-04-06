6cab80f625f711f2c09cfa7d4c914343ca7624c5
coob <koosanghwi@gmail.com>
2025-04-06 10:16:10 +0900

    ver 1.0802

--- data_ymir/mymodule/guild.py
+++ data_ymir/mymodule/guild.py
@@ -256,7 +256,7 @@ def guild_check(cla):
                     imgs_ = imgs_set_(350, 500, 500, 580, cla, img, 0.85)
                     if imgs_ is not None and imgs_ != False:
                         print("anymore_donation_notice", imgs_)
-                        is_get = True
+
                         break
                     else:
                         full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\guild\\donation_btn.PNG"
@@ -326,23 +326,25 @@ def guild_check(cla):
                         break
                     QTest.qWait(1000)
 
-                if is_get == True:
-                    for i in range(5):
-                        full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\action\\menu_open\\menu_post.PNG"
+                # if is_get == True:
+                for i in range(5):
+                    full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\action\\menu_open\\menu_post.PNG"
+                    img_array = np.fromfile(full_path, np.uint8)
+                    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                    imgs_ = imgs_set_(620, 550, 850, 640, cla, img, 0.7)
+                    if imgs_ is not None and imgs_ != False:
+                        break
+                    else:
+                        full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\title\\guild.PNG"
                         img_array = np.fromfile(full_path, np.uint8)
                         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                        imgs_ = imgs_set_(620, 550, 850, 640, cla, img, 0.7)
+                        imgs_ = imgs_set_(30, 30, 160, 80, cla, img, 0.9)
                         if imgs_ is not None and imgs_ != False:
-                            break
-                        else:
-                            full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\title\\guild.PNG"
-                            img_array = np.fromfile(full_path, np.uint8)
-                            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                            imgs_ = imgs_set_(30, 30, 160, 80, cla, img, 0.9)
-                            if imgs_ is not None and imgs_ != False:
-                                print("title : guild")
-                                click_pos_2(30, 50, cla)
-                        QTest.qWait(1000)
+                            print("title : guild")
+                            click_pos_2(30, 50, cla)
+                    QTest.qWait(1000)
+
+                is_get = True
 
 
 
