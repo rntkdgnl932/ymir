7b37ad50f7daad2ded983dce9e9eaec6e1f7272b
coob <koosanghwi@gmail.com>
2025-04-06 11:29:55 +0900

    ver 1.0803

--- data_ymir/mymodule/tuto.py
+++ data_ymir/mymodule/tuto.py
@@ -20,8 +20,16 @@ def tuto_start(cla):
     from massenger import line_to_me
     from dead_die import dead_check, dead_after
     from schedule import myQuest_play_add
+    from clean_screen import clean_screen_start
     try:
         print("tuto_start")
+
+        full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\action\\menu_open\\menu_post.PNG"
+        img_array = np.fromfile(full_path, np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        imgs_ = imgs_set_(650, 550, 850, 650, cla, img, 0.8)
+        if imgs_ is not None and imgs_ != False:
+            clean_screen_start(cla)
 
         juljun_off(cla)
 
