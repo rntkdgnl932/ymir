9ee4c8890f4c3e91a4b71a6b747beaed59a7582c
coob <koosanghwi@gmail.com>
2025-05-29 14:03:34 +0900

    ver 1.0841

--- data_ymir/mymodule/dungeon.py
+++ data_ymir/mymodule/dungeon.py
@@ -757,7 +757,7 @@ def dungeon_in(cla, data):
                     full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\dungeon\\dun_complete.PNG"
                     img_array = np.fromfile(full_path, np.uint8)
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(800, 240, 880, 320, cla, img, 0.85)
+                    imgs_ = imgs_set_(780, 240, 900, 320, cla, img, 0.85)
                     if imgs_ is not None and imgs_ != False:
                         print("dun_complete", imgs_)
                         complete = True
