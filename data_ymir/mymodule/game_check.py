4dc313f2f1a58b7df370e1431bdd4fd13155d886
coob <koosanghwi@gmail.com>
2025-06-19 15:45:31 +0900

    ver 1.0842

--- data_ymir/mymodule/game_check.py
+++ data_ymir/mymodule/game_check.py
@@ -75,6 +75,16 @@ def check_start(cla):
                 line_to_me(cla, why)
                 # macro_out(cla)
                 confirm_all(cla)
+            else:
+                full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\game_check\\version_different.PNG"
+                img_array = np.fromfile(full_path, np.uint8)
+                img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                imgs_ = imgs_set_(0, 60, 880, 930, cla, img, 0.7)
+                if imgs_ is not None and imgs_ != False:
+                    why = "버전이 다르다"
+                    line_to_me(cla, why)
+                    macro_out(cla)
+                    confirm_all(cla)
         # 캐릭터 선택
         full_path = "c:\\my_games\\ymir\\data_ymir\\imgs\\character_start\\login_character_title.PNG"
         img_array = np.fromfile(full_path, np.uint8)
